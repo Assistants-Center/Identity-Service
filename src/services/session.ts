15262798341e71c03e9342bd73b7328819254299
Marcin Kondrat <breftejk@gmail.com>
2023-05-29 12:19:13 +0200

    Session Service as Auth Service child

--- src/services/session.ts
+++ src/services/session.ts
@@ -6,28 +6,26 @@ import {
   RawServerBase,
   RouteGenericInterface,
 } from "fastify";
-import SessionService from "./session";
 
-class AuthService<
+declare module "fastify" {
+  interface Session {
+    user: HydratedDocument<IUser>;
+  }
+}
+
+class SessionService<
   Request extends RouteGenericInterface,
   Reply extends RawServerBase
 > {
-  private readonly sessionService: SessionService<Request, Reply>;
   constructor(
     private readonly request: FastifyRequest<Request>,
     private readonly reply: FastifyReply<Reply>,
     private readonly user: HydratedDocument<IUser>
-  ) {
-    this.sessionService = new SessionService(request, reply, user);
-  }
+  ) {}
 
-  public async login() {
-    if (this.user.two_factor.enabled) {
-      // handle 2FA
-    } else {
-      await this.sessionService.saveUser();
-    }
+  public async saveUser() {
+    this.request.session.set("user", this.user);
   }
 }
 
-export default AuthService;
+export default SessionService;
